50e0d5de272b201d0ac43f1ae4a1f4316efe6c4f
KLI0273 <126114680+KLI0273@users.noreply.github.com>
2023-05-04 14:32:08 +0200

    bool_matice

--- vvp_bludiste-master/vvp_project_support.py
+++ vvp_bludiste-master/vvp_project_support.py
@@ -2,12 +2,13 @@ import numpy as np
 import matplotlib.pylab as plt
 import csv
 
-def load_document():
-    #data = open(f'{string}', 'r')
-    data = open('vvp_bludiste-master/data/maze_1.csv', 'r')
+def load_document(string):
+    data = open(string, 'r')
     soubor = csv.reader(data, delimiter=',')
     maze = []
     for rows in soubor:
         maze.append([int(cell) for cell in rows])
-    data.close
+    data.close()
     return np.array(maze).astype(bool)
+
+
